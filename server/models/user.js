a66de5f48d27d23e942b05893eaf0f72f46276a8
ahmetucar1 <213214006@stu.adu.edu.tr>
2023-01-24 16:53:59 +0300

    edit .env

--- server/models/user.js
+++ server/models/user.js
@@ -12,7 +12,7 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.methods.generateAuthToken = function () {
-	const token = jwt.sign({ _id: this._id }, process.env.JWTPRIVATEKEY, {
+	const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET, {
 		expiresIn: "90d",
 	});
 	return token;
